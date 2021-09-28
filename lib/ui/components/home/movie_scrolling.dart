2b2905bb56808ad89fc8fbb41d7aa75df4c98cfd
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-09-28 01:17:59 -0300

    feat: movie scrolling home widget

--- lib/ui/components/home/movie_scrolling.dart
+++ lib/ui/components/home/movie_scrolling.dart
@@ -1,16 +1,30 @@
+import 'dart:math';
+
+import 'package:cached_network_image_builder/cached_network_image_builder.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+import '../../config/config.dart';
+import '../../pages/home/viewmodel/viewmodel.dart';
 
 class MovieScrolling extends StatelessWidget {
   final String title;
+  final String category;
+  final Stream<List<FilmViewModel>>? stream;
 
-  const MovieScrolling({Key? key, required this.title}) : super(key: key);
+  const MovieScrolling({
+    Key? key,
+    this.stream,
+    required this.title,
+    required this.category,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -19,7 +33,10 @@ class MovieScrolling extends StatelessWidget {
                 style: Theme.of(context).textTheme.headline6,
               ),
               TextButton(
-                onPressed: () {},
+                onPressed: () => Navigator.of(context).pushNamed(
+                  AppRoutes.VIEW_MORE_PAGE,
+                  arguments: category,
+                ),
                 child: Text(
                   "Ver mais",
                   style: Theme.of(context).textTheme.headline6!.copyWith(fontSize: 18, color: Theme.of(context).colorScheme.secondary),
@@ -28,34 +45,92 @@ class MovieScrolling extends StatelessWidget {
             ],
           ),
         ),
-        Container(
-          margin: const EdgeInsets.only(bottom: 24),
-          height: 230,
-          child: ListView.builder(
-            itemCount: 10,
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (context, index) => Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(
-                    left: 8,
-                    right: 8,
+        StreamBuilder<List<FilmViewModel>>(
+          stream: stream,
+          builder: (context, snapshot) {
+            if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+              return Container(
+                margin: const EdgeInsets.only(bottom: 24),
+                height: 230,
+                child: ListView.builder(
+                  itemCount: min(snapshot.data!.length, 10),
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) => Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      InkWell(
+                        onTap: () => Navigator.of(context).pushNamed(
+                          AppRoutes.DETAIL_PAGE,
+                          arguments: snapshot.data![index].id,
+                        ),
+                        child: Container(
+                          margin: const EdgeInsets.only(
+                            left: 18,
+                            right: 18,
+                          ),
+                          height: 190,
+                          width: 134,
+                          child: CachedNetworkImageBuilder(
+                            url: snapshot.data![index].image,
+                            builder: (img) {
+                              return Image.file(
+                                img,
+                                fit: BoxFit.cover,
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 134,
+                        child: Text(
+                          snapshot.data![index].title,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context).textTheme.headline6?.copyWith(fontSize: 14),
+                        ),
+                        margin: const EdgeInsets.only(left: 18, right: 18, top: 12),
+                      ),
+                    ],
                   ),
-                  height: 190,
-                  width: 134,
-                  color: Colors.amber,
                 ),
-                Container(
-                  child: Text(
-                    "Teste",
-                    style: Theme.of(context).textTheme.headline6?.copyWith(fontSize: 18),
+              );
+            }
+            return Shimmer.fromColors(
+              baseColor: Colors.grey[600]!,
+              highlightColor: Colors.grey[900]!,
+              child: Container(
+                color: Colors.black26,
+                margin: const EdgeInsets.only(bottom: 24),
+                height: 230,
+                child: ListView.builder(
+                  itemCount: 10,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) => Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(
+                          left: 18,
+                          right: 18,
+                        ),
+                        height: 190,
+                        width: 134,
+                      ),
+                      Container(
+                        width: 134,
+                        child: Text(
+                          "******************",
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context).textTheme.headline6?.copyWith(fontSize: 14),
+                        ),
+                        margin: const EdgeInsets.only(left: 18, right: 18, top: 12),
+                      ),
+                    ],
                   ),
-                  margin: const EdgeInsets.only(left: 8, right: 8, top: 12),
                 ),
-              ],
-            ),
-          ),
+              ),
+            );
+          },
         ),
       ],
     );
