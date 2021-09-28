ff03b545d13ddcd2fd0140907736b5f2f7c43680
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-09-28 01:04:01 -0300

    feat: remote film detail model

---
+++ lib/data/film_detail_model.dart
@@ -0,0 +1,103 @@
+import 'dart:convert';
+
+class RemoteFilmDetailModel {
+  String id;
+  String title;
+  String originalTitle;
+  String fullTitle;
+  String type;
+  String year;
+  String image;
+  String awards;
+  String plot;
+  List<String> directorList;
+  List<String> writerList;
+  List<String> starList;
+  List<Actor> actorList;
+  List<String> genreList;
+
+  RemoteFilmDetailModel({
+    required this.id,
+    required this.title,
+    required this.originalTitle,
+    required this.fullTitle,
+    required this.type,
+    required this.year,
+    required this.image,
+    required this.awards,
+    required this.plot,
+    required this.directorList,
+    required this.writerList,
+    required this.starList,
+    required this.actorList,
+    required this.genreList,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'originalTitle': originalTitle,
+      'fullTitle': fullTitle,
+      'type': type,
+      'year': year,
+      'image': image,
+      'awards': awards,
+      'directorList': directorList,
+      'writerList': writerList,
+      'starList': starList,
+      'actorList': [...actorList.map((e) => e.toMap()).toList()],
+      'genreList': genreList,
+    };
+  }
+
+  factory RemoteFilmDetailModel.fromMap(Map<String, dynamic> map) {
+    return RemoteFilmDetailModel(
+      id: map['id'] ?? "",
+      title: map['title'] ?? "",
+      plot: map['plot'] ?? "",
+      originalTitle: map['originalTitle'] ?? "",
+      fullTitle: map['fullTitle'] ?? "",
+      type: map['type'] ?? "",
+      year: map['year'] ?? "",
+      image: map['image'] ?? "",
+      awards: map['awards'] ?? "",
+      directorList: List.from(map['directorList'] ?? []).map((e) => e["name"].toString()).toList(),
+      writerList: List.from(map['writerList'] ?? []).map((e) => e["name"].toString()).toList(),
+      starList: List.from(map['starList'] ?? []).map((e) => e["name"].toString()).toList(),
+      actorList: List.from(map['actorList'] ?? []).map((e) => Actor.fromMap(e)).toList(),
+      genreList: List.from(map['genreList'] ?? []).map((e) => e["value"].toString()).toList(),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory RemoteFilmDetailModel.fromJson(String source) => RemoteFilmDetailModel.fromMap(json.decode(source));
+}
+
+class Actor {
+  String id;
+  String image;
+  String name;
+  Actor({
+    required this.id,
+    required this.image,
+    required this.name,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'image': image,
+      'name': name,
+    };
+  }
+
+  factory Actor.fromMap(Map<String, dynamic> map) {
+    return Actor(
+      id: map['id'],
+      image: map['image'],
+      name: map['name'],
+    );
+  }
+}
