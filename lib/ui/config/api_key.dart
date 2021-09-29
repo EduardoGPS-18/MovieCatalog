4c68cc93bbe8d4b6a1d339b0faaa889902b5301e
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-09-29 17:56:06 -0300

    fix: change api key

--- lib/ui/config/api_key.dart
+++ lib/ui/config/api_key.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: constant_identifier_names
 
 // const String API_KEY = "k_b7jbo2n6";
-const String API_KEY = "k_r1ptause";
-// const String API_KEY = "k_aiic0jlr";
+// const String API_KEY = "k_r1ptause";
+const String API_KEY = "k_aiic0jlr";
 // const String API_KEY = "k_938roqrr";
