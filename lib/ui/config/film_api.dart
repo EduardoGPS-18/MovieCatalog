a4519c3a4f0ef5f706cd790bcf2daa6c5d793138
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-09-29 16:52:40 -0300

    fix: convert methods into consts

--- lib/ui/config/film_api.dart
+++ lib/ui/config/film_api.dart
@@ -4,6 +4,6 @@ import 'api_key.dart';
 
 class FilmApi {
   static const RELEASES_FILM = "https://imdb-api.com/br/API/ComingSoon/$API_KEY";
-  static String getApiPathByCategory(String category) => "https://imdb-api.com/br/API/Keyword/$API_KEY/$category";
-  static String getApiPathByFilmId(String id) => "https://imdb-api.com/en/API/Title/$API_KEY/$id";
+  static const String apiPathToSearchFilmsByCategory = "https://imdb-api.com/br/API/Keyword/$API_KEY";
+  static const String apiPathToSearchFilmInformation = "https://imdb-api.com/en/API/Title/$API_KEY";
 }
