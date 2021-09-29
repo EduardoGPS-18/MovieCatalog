c7e412d404e7a5bd9f2807570fa2706e2a555d6e
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-09-29 14:21:58 -0300

    feat: Implementede load releases usecasae

---
+++ lib/data/usecases/remote_load_releases_films.dart
@@ -0,0 +1,19 @@
+import '../../domain/domain.dart';
+import '../../ui/pages/home/viewmodel/release_film_viewmodel.dart';
+import '../data.dart';
+
+class RemoteLoadReleasesFilms implements LoadReleasesFilm {
+  final HTTPClient client;
+  final String url;
+
+  RemoteLoadReleasesFilms({
+    required this.client,
+    required this.url,
+  });
+
+  @override
+  Future<List<ReleasFilmEntity>> loadReleasesFilm() async {
+    List<dynamic> res = await client.request(url: url, method: HTTPMethod.get);
+    return res.map((film) => RemoteReleaseFilmModel.fromJson(film).toEntity()).toList();
+  }
+}
