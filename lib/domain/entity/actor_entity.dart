e2176d36c04c3f2e07fff190936981ae0a3aa001
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-09-29 15:43:10 -0300

    refactor: added equetable by inheritance to actor entity

--- lib/domain/entity/actor_entity.dart
+++ lib/domain/entity/actor_entity.dart
@@ -1,10 +1,15 @@
-class ActorEntity {
-  String id;
-  String image;
-  String name;
-  ActorEntity({
+import 'package:equatable/equatable.dart';
+
+class ActorEntity extends Equatable {
+  final String id;
+  final String image;
+  final String name;
+  const ActorEntity({
     required this.id,
     required this.image,
     required this.name,
   });
+
+  @override
+  List<Object?> get props => [id, image, name];
 }
