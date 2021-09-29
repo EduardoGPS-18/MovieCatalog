e5d7192ff400d0fd1b029640e24fbb8293bf8e2d
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-09-29 14:09:43 -0300

    feat: added remote load film by category (load film by category impl)

---
+++ lib/data/usecases/remote_load_film_by_category.dart
@@ -0,0 +1,20 @@
+import '../../domain/entity/film_entity.dart';
+import '../../domain/usecases/usecases.dart';
+import '../data.dart';
+
+class RemoteLoadFilmByCategory implements LoadFilmByCategory {
+  final HTTPClient httpClient;
+  final String url;
+
+  RemoteLoadFilmByCategory({
+    required this.httpClient,
+    required this.url,
+  });
+
+  @override
+  Future<List<FilmEntity>> loadFilmsByCategory() async {
+    final List<dynamic> response = await httpClient.request(url: url, method: HTTPMethod.get);
+
+    return response.map((film) => RemoteFilmModel.fromJson(film).toEntity()).toList();
+  }
+}
---
+++ lib/data/usecases/usecases.dart
@@ -0,0 +1,1 @@
+export 'remote_load_film_by_category.dart';
