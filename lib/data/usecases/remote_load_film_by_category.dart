5c4bdeaf2a88b209540d330dcdb0a05c6907f564
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-09-29 16:21:55 -0300

    refactor: change variable name httpClient to client

--- lib/data/usecases/remote_load_film_by_category.dart
+++ lib/data/usecases/remote_load_film_by_category.dart
@@ -3,17 +3,17 @@ import '../../domain/usecases/usecases.dart';
 import '../data.dart';
 
 class RemoteLoadFilmByCategory implements LoadFilmByCategory {
-  final HTTPClient httpClient;
+  final HTTPClient client;
   final String url;
 
   RemoteLoadFilmByCategory({
-    required this.httpClient,
+    required this.client,
     required this.url,
   });
 
   @override
   Future<List<FilmEntity>> loadFilmsByCategory({required String category}) async {
-    final List<dynamic> response = await httpClient.request(url: url, method: HTTPMethod.get);
+    final List<dynamic> response = await client.request(url: url, method: HTTPMethod.get);
 
     return response.map((film) => RemoteFilmModel.fromJson(film).toEntity()).toList();
   }
